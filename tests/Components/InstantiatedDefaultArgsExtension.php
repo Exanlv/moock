daf0db58f006e9399a086060cbc46ccb6021e14a
Exanlv <51094537+Exanlv@users.noreply.github.com>
2026-07-05 15:16:31 +0200

    cs

--- tests/Components/InstantiatedDefaultArgsExtension.php
+++ tests/Components/InstantiatedDefaultArgsExtension.php
@@ -4,7 +4,4 @@ declare(strict_types=1);
 
 namespace Tests\Components;
 
-class InstantiatedDefaultArgsExtension extends InstantiatedDefaultArgsBaseClass
-{
-
-}
+class InstantiatedDefaultArgsExtension extends InstantiatedDefaultArgsBaseClass {}
